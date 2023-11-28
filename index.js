3f7fbe08f3b56318d0cac892ab7a67f4d1fe5395
Rakibulislam1 <rirakib03@gmail.com>
2023-11-29 05:28:55 +0600

    make admin

--- index.js
+++ index.js
@@ -84,6 +84,19 @@ async function run() {
       res.send(result);
     });
 
+      //make Admin
+      app.patch("/users/role/:id", async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: "Admin",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
