8e3dc551ed9d07d06ed360d4011f918f308600a5
Rakibulislam1 <rirakib03@gmail.com>
2023-11-29 05:33:02 +0600

    get all article & get premium article

--- index.js
+++ index.js
@@ -124,6 +124,113 @@ async function run() {
         res.send(result);
       });
 
+          //approved
+    app.patch("/add-articles/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+
+      const query = { _id: new ObjectId(id) };
+
+      const updateDoc = {
+        $set: {
+          status: data.status,
+        },
+      };
+      const result = await dailyNewsCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    //DashboardDecline post
+    app.post("/decline", async (req, res) => {
+      const article = req.body;
+      const result = await dashboardDecline.insertOne(article);
+      res.send(result);
+    });
+
+    //DashboardDecline get
+    app.get("/decline", async (req, res) => {
+      const result = await dashboardDecline.find().toArray();
+      res.send(result);
+    });
+
+    //my article
+    app.get("/add-articles/myArticle", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await dailyNewsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //my article delete
+    app.delete("/add-articles/myArticle/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await dailyNewsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
+    
+    //my article get
+    app.get('/add-articles/update/:id',async(req,res) =>{
+      const id = req.params.id 
+      const query = {_id :new ObjectId(id)}
+      const result = await dailyNewsCollection.findOne(query)
+      res.send(result)
+    })
+
+
+    //my article update
+    app.patch("/add-articles/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          title: data.title,
+          image: data.image,
+          publisher: data.publisher,
+          tag: data.tag,
+          description: data.description,
+        },
+      };
+      const result = await dailyNewsCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    // all article get
+    app.get("/add-articles/approve", async (req, res) => {
+      const query = { status: "Approve" };
+      const result = await dailyNewsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // get view details
+    app.get('/add-articles/viewDetails/:id',async(req,res) =>{
+      const id = req.params.id 
+      const query = {_id :new ObjectId(id)}
+      const result = await dailyNewsCollection.findOne(query)
+      res.send(result)
+    })
+
+
+    // all article get
+    app.get("/add-articles/approve", async (req, res) => {
+      const query = { status: "Approve" };
+      const result = await dailyNewsCollection.find(query).toArray();
+      res.send(result);
+    });
+    
+
+    // premium article get
+    app.get("/premiumArticle", async (req, res) => {
+      const plan = req.query.plan;
+      const query = { plan: plan };
+      const result = await dailyNewsCollection.find(query).toArray();
+      console.log(result);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
